09dd0357bd05a6a85d48d18982dd5b538dd9bfbb
Jolisa Tran <jolisa.chan.store@gmail.com>
2020-04-23 00:29:01 -0400

    update Customer.java with more fields

--- src/com/mywebsite/Customer.java
+++ src/com/mywebsite/Customer.java
@@ -3,23 +3,40 @@ package com.mywebsite;
 import java.io.Serializable;
 
 public class Customer implements Serializable{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private String fName;
 	private String lName;
+	private String street;
+	private String city;
+	private String state;
+	private String zipcode;
+	private String email;
+	private String password;
 	private String phone;
 	
 	public Customer() {
 		this.fName = "";
 		this.lName = "";
+		this.street = "";
+		this.city = "";
+		this.state = "";
+		this.zipcode = "";
+		this.email = "";
+		this.password = "";
 		this.phone = "";
 	}
 	
-	public Customer(String fName, String lName, String phone) {
+	public Customer(String fName, String lName, String street,
+			String city, String state, String zipcode,
+			String email, String password, String phone) {
 		this.fName = fName;
 		this.lName = lName;
+		this.setStreet(street);
+		this.setCity(city);
+		this.setState(state);
+		this.setZipcode(zipcode);
+		this.setEmail(email);
+		this.setPassword(password);
 		this.phone = phone;
 	}
 	
@@ -41,6 +58,54 @@ public class Customer implements Serializable{
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
  
+	public String getStreet() {
+		return street;
+	}
+ 
+	public void setStreet(String street) {
+		this.street = street;
+	}
+ 
+	public String getCity() {
+		return city;
+	}
+ 
+	public void setCity(String city) {
+		this.city = city;
+	}
+ 
+	public String getState() {
+		return state;
+	}
+ 
+	public void setState(String state) {
+		this.state = state;
+	}
+ 
+	public String getZipcode() {
+		return zipcode;
+	}
+ 
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+ 
+	public String getEmail() {
+		return email;
+	}
+ 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+ 
+	public String getPassword() {
+		return password;
+	}
+ 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
+ 
